abda58cc414b9350baf3e3f651cfbf98d09fb56c
Mitchell R <mitchell.robert@bettercorp.co.za>
2023-04-09 05:18:42 +0200

    feat(index.ts): add exports for splynx plugin and related types and classes

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export { splynx } from "./clients/service-splynx/plugin";
+export { IServerConfig } from "./plugins/service-splynx/sec.config";
+export {
+  SplynxReturnEmitEvents,
+  SplynxEmitAndReturnEvents,
+  SplynxReturnEmitAndReturnEvents,
+} from "./plugins/service-splynx/plugin";
+export {
+  SplynxPayment,
+  SplynxPaymentMethod,
+  SplynxClient,
+  SplynxInvoice,
+} from "./plugins/service-splynx/lib/splynx";
